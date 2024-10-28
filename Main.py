fa88e99a48a2d7575ab364eef5214bff08159995
Carson <carsonburke22@gmail.com>
2024-10-28 09:34:41 -0700

    basic pygame setup

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
-print("test")
+import pygame as pg
+
+# Constants
+SCREENRECT = pg.Rect(0, 0, 640, 480)
+
+# Variables
+tick = 0
+
+def main():
+    print("start")
+
+    pg.init()
+
+    fullscreen = True
+    winstyle = 0
+    bestdepth = pg.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pg.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+
+    background = pg.Surface(SCREENRECT.size)
+    screen.blit(background, (0, 0))
+
+    clock = pg.time.clock()
+
+    while True:
+        loop(clock)
+
+main()
+
+def loop(clock):
+    print("loop")
+
+    clock.tick(60)
+
+    tick += 1
